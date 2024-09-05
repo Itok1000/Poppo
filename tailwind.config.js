6e05bde0e82378b10982366ea95a0d1e5efcdd2f
伊藤健太 <jiantaiyiteng1@gmail.com>
2024-09-05 10:23:29 +0900

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,4 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ]
-
-  corePlugins: {
-    preflight: false, // もしプリフライトを使っていない場合
-  },
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
-  // ソースマップ無効化
-  important: true, // 既に指定されている場合、他のルールを上書きするため
 }
